26da2ddd4a5490c334c1597a445b80c0569a83ec
Nuhota-HoangThai <nguyenhoangthai0134@gmail.com>
2024-04-17 00:26:09 +0700

    16/04

--- routes/StatisticalRoute.js
+++ routes/StatisticalRoute.js
@@ -7,55 +7,55 @@ const { verifyTokenCus } = require("../middleware/verifyTokenCus");
 // Route thống kê tổng số tour
 router.get(
   "/total-tours",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.totalTours
 );
 
 // Route thống kê số lượng tour đã bán được
 router.get(
   "/tours-sold",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.toursSold
 );
 
 // Route thống kê tổng số reviews
 router.get(
   "/total-reviews",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.totalReviews
 );
 
 // Ví dụ: /new-customers?startDate=2023-01-01&endDate=2023-01-31
 router.get(
   "/new-customers",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.newCustomers
 );
 
 // Route for daily revenue
 router.get(
   "/daily-revenue",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.revenueByDay
 );
 
 // Example: /monthly-revenue?year=2023&month=01
 router.get(
   "/monthly-revenue",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.revenueByMonth
 );
 
 // Example: /yearly-revenue?year=2023
 router.get(
   "/yearly-revenue",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.revenueByYear
 );
 
 router.get(
   "/booking-stats",
-  verifyTokenCus(["admin"]),
+  verifyTokenCus(["admin", "staff"]),
   StatisticalController.bookingStatusStatistics
 );
 
